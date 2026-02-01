4cd3a5fe6c35211c3e07ae4e18633e93cee1041a
Dilshan Desktop <narankotuwadilshan@gmail.com>
2026-02-01 22:41:42 +0530

    add strength section to homepage

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header'
 import HeroSection from "../components/HeroSection"
+import StrengthSection from '../components/StrengthSection';
 
 export default function  HomePage() {
 return (
     <>
       <Header/>
       <HeroSection/>
+      <StrengthSection/>
     </>
 );
 }
