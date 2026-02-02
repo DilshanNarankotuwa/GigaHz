ec88e70a07f1accfd65e3121e902083cedae841c
Dilshan Desktop <narankotuwadilshan@gmail.com>
2026-02-02 09:39:09 +0530

    add preowned page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import HomePage from './pages/homepage'
 import LaptopsPage from './pages/laptopspage'
-import MobilesPage from './pages/MobilesPage'
+import MobilesPage from './pages/mobilespage'
+import PreOwnedPage from './pages/preownedpage'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route index element={<HomePage/>}/>
       <Route path='laptops' element={<LaptopsPage/>}/>
       <Route path='mobiles' element={<MobilesPage/>}/>
+      <Route path='preowned' element={<PreOwnedPage/>}/>
     </Routes>
   )
 }
